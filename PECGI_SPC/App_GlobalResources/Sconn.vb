4f41680e4c4e742d00cab4d5c359047247db4a57
Ari <ari@tos.co.id>
2022-09-28 17:56:42 +0700

    <Upd><Sconn><IOT Connection String>

--- PECGI_SPC/App_GlobalResources/Sconn.vb
+++ PECGI_SPC/App_GlobalResources/Sconn.vb
@@ -1,5 +1,9 @@
 ﻿Public Class Sconn
     Public Shared Function Stringkoneksi() As String
         Return ConfigurationManager.ConnectionStrings("ApplicationServices").ConnectionString
-    End Function   
+    End Function
+
+    Public Shared Function IOTConnectionString() As String
+        Return ConfigurationManager.ConnectionStrings("IOTSystem").ConnectionString
+    End Function
 End Class
